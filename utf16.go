a1627ca6b576f4f18988dbaa56d33ab5367ac3ea
Simon Biewald <simon@fam-biewald.de>
2021-10-14 22:26:48 +0200

    Fix pointer misuse and add comments.
    1. The result of uintptr(unsafe.Pointer(...)) may not be stored in a
    variable, as defined at https://pkg.go.dev/unsafe#Pointer (rule 3).
    2. Clarify UTF16PtrToString returns an empty string if the UTF-16
    sequence is larger then maxLen.

--- utf16.go
+++ utf16.go
@@ -5,18 +5,25 @@ import (
 	"unsafe"
 )
 
-// UTF16PtrToString converts a pointer to a UTF16 string to a string
+// UTF16PtrToString converts a pointer to a UTF-16 sequence to a string.
+// The UTF-16 sequence must null terminated or shorter than maxLen.
+//
+// If the UTF-16 sequence is longer than maxlen, an empty string is returned.
 func UTF16PtrToString(ptr *uint16, maxLen int) (s string) {
 	if ptr == nil {
 		return ""
 	}
+
 	buf := make([]uint16, 0, maxLen)
-	for i, p := 0, uintptr(unsafe.Pointer(ptr)); i < maxLen; i, p = i+1, p+2 {
-		char := *(*uint16)(unsafe.Pointer(p))
+	for i, p := 0, unsafe.Pointer(ptr); i < maxLen; i, p = i+1, unsafe.Pointer(uintptr(p)+2) {
+		char := *(*uint16)(p)
 		if char == 0 {
+			// Decode sequence and return
 			return string(utf16.Decode(buf))
 		}
+
 		buf = append(buf, char)
 	}
+	// Return empty string once maxLen is reached.
 	return ""
 }
